22626ba803c1f8b0812884e6940d10eb829e9dbb
Matheus Lago Pereira <m.pereira96@hotmail.com>
2016-09-14 14:34:51 -0300

    Include express session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 var room = require('./routes/room');
@@ -20,6 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'matheusevitor1321312312321', // just a long random string
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public'), {
   extensions: ['html']
 }));
