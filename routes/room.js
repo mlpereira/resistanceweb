7d81a6fb88743667c2ea697bfc553ac29cff0272
vitorvanacor <vitor_vanacor@hotmail.com>
2016-10-07 15:58:33 -0300

    add bots, start game

--- routes/room.js
+++ routes/room.js
@@ -3,37 +3,36 @@ var router = express.Router();
 
 var rooms = {};
 
-function Room(id, sessionId, name) {
+function Player(id, name){
   this.id = id;
-  this.owner = sessionId;
-  this.players = {};
-  this.players[sessionId] = {
-    id: sessionId,
-    name: name
-  }
-  this.phase =0;
+  this.name = name;
 }
 
-router.post('/', function(req, res, next) {
-  do {
-    var roomId = 10; //Math.floor((Math.random() * 100000) + 1);
-  } while(rooms[roomId] != undefined)
+function Room(id, player) {
+  this.id = id;
+  this.owner = player;
+  this.players = [];
+  this.players.push(player);
+  this.phase = 0;
+  this.getPlayer = function(playerId){
+    for(var i=0; i<this.players.length; i++){
+      if (this.players[i].id == playerId)
+        return this.players[i];
+      return false;
+    }
+  };
+}
 
-  rooms[roomId] = new Room(roomId, req.session.id, req.body.username);
-
-  res.redirect('/room/' + roomId)
-})
-
-router.get('/:roomId', function(req, res, next) {
-  var roomId = req.params.roomId
-  var room = rooms[roomId]
+function getRoom(req, res, next){
+  var roomId = req.params.roomId;
+  var room = rooms[roomId];
 
   if (!room) {
     var err = new Error('Not Found');
     err.status = 404;
-    return next(err)
+    return next(err);
   }
-  var player = room.players[req.session.id];
+  var player = room.getPlayer(req.session.id);
   switch (room.phase) {
     case 0:
       res.render('room', {room, player});
@@ -42,39 +41,65 @@ router.get('/:roomId', function(req, res, next) {
       res.render('phaseOne', {room, player});
       break;
   }
-});
+}
 
-router.post('/:roomId/joinRoom', function(req, res, next) {
-  var room = rooms[req.params.roomId]
+function createRoom(req, res, next){
+  do {
+    var roomId = 10; //Math.floor((Math.random() * 100000) + 1);
+  } while(rooms[roomId] != undefined)
+  var player = new Player(req.session.id, req.body.username);
+  rooms[roomId] = new Room(roomId, player);
+
+  res.redirect('/room/' + roomId);
+}
+
+function joinRoom(req, res, next){
+  var room = rooms[req.params.roomId];
   if (!room) {
     var err = new Error('Not Found');
     err.status = 404;
-    return next(err)
+    return next(err);
   }
 
-  var player =  {
-    id: req.session.id,
-    name: req.body.username,
-  }
-  room.players[player.id] = player;
+  var player = new Player(req.session.id, req.body.username);
+  room.players.push(player);
 
-  res.redirect('/room/' + room.id)
-});
+  res.redirect('/room/' + room.id);
+}
 
-router.post('/:roomId/startGame', function(req, res, next){
-  var room = rooms[req.params.roomId]
+function startGame(req, res, next){
+  var room = rooms[req.params.roomId];
   if (!room) {
     var err = new Error('Not Found');
     err.status = 404;
-    return next(err)
+    return next(err);
   }
 
   room.phase = 1;
-  for (var key in room.players){
-    room.players[key].isLoyal = (key % 2 == 0 ? true : false)
-  }
+  assignRoles(room.players);
+  res.redirect('/room/' + room.id);
+}
 
-  res.redirect('/room/' + room.id)
-})
+function assignRoles(players){
+  for (var i=0; i<players.length; i++){
+    players[i].isLoyal = (i % 2 == 0 ? true : false);
+  }
+}
+
+function addBots(req, res, next){
+  var room = rooms[req.params.roomId];
+  for (var i = 1; i <= 4; i++){
+    var bot = new Player(i, 'BOT'+i);
+    room.players.push(bot);
+  }
+  res.redirect('/room/' + room.id);
+}
+
+router.get('/:roomId', getRoom);
+
+router.post('/', createRoom);
+router.post('/:roomId/joinRoom', joinRoom);
+router.post('/:roomId/startGame', startGame);
+router.post('/:roomId/addBots', addBots);
 
 module.exports = router;
